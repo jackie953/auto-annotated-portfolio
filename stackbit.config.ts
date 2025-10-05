ccbb9930c7a807931939b51e0a17bfc84f137a96
jackie953 <jackie@mhitaryan.com>
2025-10-05 08:45:58 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,20 +1,32 @@
-import { defineStackbitConfig } from '@stackbit/types';
+import { defineStackbitConfig } from "@stackbit/types";
 
 export default defineStackbitConfig({
-  stackbitVersion: '~0.6.0',
-  ssgName: 'nextjs',
-  nodeVersion: '18',
+  stackbitVersion: "~0.6.0",
+  ssgName: "nextjs",
+  nodeVersion: "18",
   contentSources: [
     {
-      type: 'git',
+      type: "filesystem",
+      name: "git-files",
       rootPath: __dirname,
-      contentDirs: ['content'],
-      models: [],
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "Page",
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.md",
+          fields: [
+            { name: "title", type: "string", required: true },
+            { name: "body", type: "markdown" }
+          ]
+        }
+      ],
       assetsConfig: {
-        referenceType: 'static',
-        staticDir: 'public',
-        uploadDir: 'images',
-        publicPath: '/'
+        referenceType: "static",
+        staticDir: "public",
+        uploadDir: "images",
+        publicPath: "/"
       }
     }
   ]
