8dcce05bc69ab4730bdd16b321410c3f437f0f9d
jackie953 <jackie@mhitaryan.com>
2025-10-05 05:40:40 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,28 +1,35 @@
+// stackbit.config.ts
 import { defineStackbitConfig } from '@stackbit/types';
 import { GitContentSource } from '@stackbit/cms-git';
-import { allModels } from './.stackbit/models';
 
-const config = defineStackbitConfig({
-    stackbitVersion: '~0.7.0',
-    ssgName: 'nextjs',
-    nodeVersion: '18',
-    contentSources: [
-        new GitContentSource({
-            rootPath: __dirname,
-            contentDirs: ['content'],
-            models: allModels,
-            assetsConfig: {
-                referenceType: 'static',
-                staticDir: 'public',
-                uploadDir: 'images',
-                publicPath: '/'
-            }
-        })
-    ],
-    presetSource: {
-        type: 'files',
-        presetDirs: ['./.stackbit/presets']
-    },
-    styleObjectModelName: 'ThemeStyle'
+export default defineStackbitConfig({
+  stackbitVersion: '~0.7.0',
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      // your content lives in the "content" folder
+      contentDirs: ['content'],
+      // tell Stackbit how your pages are stored: Markdown files in content/pages
+      models: [
+        {
+          name: 'Page',
+          type: 'page',
+          // /content/pages/index.md -> "/" ; /content/pages/info.md -> "/info"
+          urlPath: '/{slug}',
+          filePath: 'content/pages/{slug}.md',
+          fields: [
+            { name: 'title', type: 'string', required: true },   // frontmatter "title"
+            { name: 'body', type: 'markdown' }                   // the Markdown body
+          ]
+        }
+      ],
+      // static assets (if/when you upload images from the editor)
+      assetsConfig: {
+        referenceType: 'static',
+        staticDir: 'public',
+        uploadDir: 'images',
+        publicPath: '/'
+      }
+    })
+  ]
 });
-export default config;
